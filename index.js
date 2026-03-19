780cdb4d69f86499433983b7a9dd2e643a59c814
anand <aanuprajapat235@gmail.com>
2026-03-19 14:26:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+     alert ("you are welcomed by anand prajapat")
+
+    let age =x;
+result == age>=18 ? "adult" : "not adult";
+
+if (age >= 18) 
+ {
+alert("adult")
+}
+else
+    {
+    alert("not adult")
+}
